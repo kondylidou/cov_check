b3deb7b438efaa77d0c44c8155019e0dc9d6bc88
kondylidou <kondylidou@cip.ifi.lmu.de>
2020-09-02 22:01:20 +0200

    guiz 10 first

--- src/Handler/Quiz.hs
+++ src/Handler/Quiz.hs
@@ -19,22 +19,19 @@ import Yesod.Form.Jquery
 
 import Form.Bootstrap3
 
-
 data LargeData = LargeData {
     textField1       :: Text,
-    intField1        :: Int,
-    doubleField1     :: Double,
-    textAreaField1   :: Textarea,
-    hiddenField1     :: Text,
-    passwordField1   :: Text,
-    emailField1      :: Text,
-    htmlField1       :: Html,
-    dayField1        :: Day,
-    timeField1       :: TimeOfDay,
-    searchField1     :: Text,
-    urlField1        :: Text,
-    selectField1     :: Bool,
-    checkboxField1   :: Bool
+    age              :: Int,
+    living           :: Bool,
+    care             :: Bool,
+    work             :: Bool,
+    smoke            :: Bool,
+    pregnancy        :: Bool,
+    conf             :: Bool,
+    fever            :: Bool,
+    fever4           :: Bool,
+    checkboxField1   :: Bool,
+    color            :: [Text]
     }
 
 
@@ -60,15 +57,10 @@ data Questions = Questions
     deriving Show
 
 
-
-data Color = Red | Blue | Gray | Black
-    deriving (Show, Eq, Enum, Bounded)
-
-
 hConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 2), submit = "Create user" }
 iConfig = BootstrapFormConfig { form = BootstrapInlineForm, submit = "Create user"}
 bConfig = BootstrapFormConfig { form = BootstrapBasicForm, submit = "Create user" }
-largeFormConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 2), submit = "Submit large data" }
+largeFormConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 4), submit = "Submit large data" }
 
 bootstrapFieldHelper config label placeholder = bootstrapFieldSettings config label Nothing placeholder Nothing Nothing
 
@@ -77,35 +69,37 @@ personHForm = renderBootstrap hConfig $ Person
     <$> areq textField (bootstrapFieldHelper hConfig "Name" (Just "Person name")) Nothing
     <*> areq textField (bootstrapFieldHelper hConfig "Surname" (Just "Person surname")) Nothing
 
+
+
 personIForm :: Html -> MForm Handler (FormResult Person, Widget)
 personIForm = renderBootstrap iConfig $ Person
     <$> areq textField (bootstrapFieldHelper iConfig "Name" (Just "Person name")) Nothing
     <*> areq textField (bootstrapFieldHelper iConfig "Surname" (Just "Person surname")) Nothing
+
 
 personForm :: Html -> MForm Handler (FormResult Person, Widget)
 personForm = renderBootstrap bConfig $ Person
     <$> areq textField (bootstrapFieldHelper bConfig "Name" (Just "Person name")) Nothing
     <*> areq textField (bootstrapFieldHelper bConfig "Surname" (Just "Person surname")) Nothing
 
+
 largeDataForm :: Html -> MForm Handler (FormResult LargeData, Widget)
 largeDataForm = renderBootstrap largeFormConfig $ LargeData
     <$> areq textField (bootstrapFieldHelper hConfig "Text" (Just "Some text content")) Nothing
-    <*> areq intField (bootstrapFieldHelper hConfig "Int" (Just "Some integer value")) Nothing
-    <*> areq doubleField (bootstrapFieldHelper hConfig "Double" (Just "Some double value")) Nothing
-    <*> areq textareaField (bootstrapFieldHelper hConfig "Area" (Just "Some text area content")) Nothing
-    <*> areq hiddenField (bootstrapFieldHelper hConfig "Hidden" (Just "Hidden field")) Nothing
-    <*> areq passwordField (bootstrapFieldHelper hConfig "Password" (Just "Password field")) Nothing
-    <*> areq emailField (bootstrapFieldHelper hConfig "Email" (Just "Email field")) Nothing
-    <*> areq htmlField (bootstrapFieldHelper hConfig "Html" (Just "Some HTML")) Nothing
-    <*> areq dayField (bootstrapFieldHelper hConfig "Day" (Just "Some day")) Nothing
-    <*> areq timeField (bootstrapFieldHelper hConfig "Time" (Just "Some time")) Nothing
-    <*> areq (searchField False) (bootstrapFieldHelper hConfig "Search" (Just "Some search")) Nothing
-    <*> areq urlField (bootstrapFieldHelper hConfig "URL" (Just "Some URL")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Bool" (Just "Some bool")) Nothing
+    <*> areq intField (bootstrapFieldHelper hConfig "How old are you?" (Just "0")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Are you living alone?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "At least once a week, do you privately care for people with age-related conditions, chronic illnesses, or frailty?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Do you work in the medical field or in a community facility?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Do you smoke?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Is there a pregnancy possibility?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Have you had close contact with a confirmed case?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "In the past 4 days, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
     <*> areq checkBoxField (bootstrapFieldHelper hConfig "Checkbox" (Just "Some checkbox")) Nothing
-
-
-
+    <*> areq (multiSelectFieldList $ colors) "Color" Nothing
+    where
+        colors :: [Text]
+        colors = ["Red", "Black"]
 
 -- Declare the form. The type signature is a bit intimidating, but here's the
 -- overview:
