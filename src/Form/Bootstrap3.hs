78eb38430bcced1afdfc11f1b0ba446a32838eae
Kevin Nsieyanji Tchokodeu <nsieyanjitchokodeu@cip.ifi.lmu.de>
2020-09-02 10:19:38 +0200

    insert form with bootstrap 3

---
+++ src/Form/Bootstrap3.hs
@@ -0,0 +1,103 @@
+
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Form.Bootstrap3 (renderBootstrap,
+                        bootstrapFieldSettings,
+                        BootstrapFormConfig (..),
+                        GridOptions (..),
+                        BootstrapForm (..)
+                        ) where
+
+import              Yesod hiding (renderBootstrap)
+
+import              Data.Maybe (listToMaybe, fromMaybe)
+
+import              Data.Text (Text, pack)
+
+import              Text.Blaze.Html
+
+data GridOptions = ColXs Int | ColSm Int | ColMd Int | ColLg Int
+
+instance Show GridOptions where
+    show (ColXs 0) = ""
+    show (ColXs columns) = "col-xs-" ++ show columns
+
+    show (ColSm 0) = ""
+    show (ColSm columns) = "col-sm-" ++ show columns
+
+    show (ColMd 0) = ""
+    show (ColMd columns) = "col-md-" ++ show columns
+
+    show (ColLg 0) = ""
+    show (ColLg columns) = "col-lg-" ++ show columns
+
+instance ToMarkup GridOptions where
+    toMarkup = toMarkup . show
+
+toOffset :: GridOptions -> String
+toOffset (ColXs 0) = ""
+toOffset (ColSm 0) = ""
+toOffset (ColMd 0) = ""
+toOffset (ColLg 0) = ""
+toOffset (ColXs columns) = "col-xs-offset-" ++ show columns
+toOffset (ColSm columns) = "col-sm-offset-" ++ show columns
+toOffset (ColMd columns) = "col-md-offset-" ++ show columns
+toOffset (ColLg columns) = "col-lg-offset-" ++ show columns
+
+data BootstrapForm = BootstrapBasicForm | BootstrapInlineForm
+    | BootstrapHorizontalForm { containerOffset :: GridOptions, container :: GridOptions, label :: GridOptions }
+
+data BootstrapFormConfig = BootstrapFormConfig { form :: BootstrapForm, submit :: String }
+
+bootstrapFieldSettings :: BootstrapFormConfig -> SomeMessage site -> Maybe (SomeMessage site)
+    -> Maybe Text -> Maybe Text -> Maybe Text -> FieldSettings site
+bootstrapFieldSettings formConfig msg tooltip placeholder id name =
+    FieldSettings msg tooltip id name (attrsFromFormConfig formConfig placeholder)
+
+attrsFromFormConfig :: BootstrapFormConfig -> Maybe Text -> [(Text, Text)]
+attrsFromFormConfig _ Nothing = [("class", "form-control")]
+attrsFromFormConfig _ (Just placeholder) = [("class", "form-control"), ("placeholder", placeholder)]
+
+renderBootstrap :: Monad m => BootstrapFormConfig -> FormRender m a
+renderBootstrap formConfig aform fragment = do
+    (res, views') <- aFormToForm aform
+    let views = views' []
+        has (Just _) = True
+        has Nothing  = False
+        widget = [whamlet|
+                        \#{fragment}
+                        $forall view <- views
+                          <div .form-group :fvRequired view:.required :not $ fvRequired view:.optional :has $ fvErrors view:.error>
+                            $case (form formConfig)
+                              $of BootstrapBasicForm
+                                <label for=#{fvId view}>#{fvLabel view}
+                                ^{fvInput view}
+                                ^{helpWidget view}
+                              $of BootstrapInlineForm
+                                <label .sr-only for=#{fvId view}>#{fvLabel view}
+                                ^{fvInput view}
+                                ^{helpWidget view}
+                              $of BootstrapHorizontalForm containerOffset containerClass labelClass
+                                <label .control-label .#{labelClass} for=#{fvId view}>#{fvLabel view}
+                                <div .#{containerClass}>
+                                  ^{fvInput view}
+                                ^{helpWidget view}
+
+                        ^{submitWidget $ formConfig}
+                |]
+    return (res, widget)
+
+submitWidget (BootstrapFormConfig (BootstrapHorizontalForm containerOffset containerClass labelClass) submit) = [whamlet|
+<div .form-group>
+    <div .#{toOffset containerOffset} .#{containerClass}>
+      <button type=submit .btn .btn-default>#{submit}
+|]
+submitWidget (BootstrapFormConfig _ submit) = [whamlet|<button type=submit .btn .btn-default>#{submit}|]
+
+helpWidget view = [whamlet|
+    $maybe tt <- fvTooltip view
+      <span .help-block>#{tt}
+    $maybe err <- fvErrors view
+      <span .help-block>#{err}
+|]
--- src/Handler/Quiz.hs
+++ src/Handler/Quiz.hs
@@ -8,25 +8,50 @@
 
 module Handler.Quiz where
 
-import Import
+import Import hiding (renderBootstrap)
+
 import Control.Applicative ((<$>), (<*>))
 import Data.Text           (Text)
-import Data.Time           (Day)
+import Data.Time           (Day, TimeOfDay (..))
 import Data.Vector() 
-import Yesod()
-import Yesod.Form.Jquery()
+import Yesod.Default.Util
+import Yesod.Form.Jquery
+
+import Form.Bootstrap3
+
+
+data LargeData = LargeData {
+    textField1       :: Text,
+    intField1        :: Int,
+    doubleField1     :: Double,
+    textAreaField1   :: Textarea,
+    hiddenField1     :: Text,
+    passwordField1   :: Text,
+    emailField1      :: Text,
+    htmlField1       :: Html,
+    dayField1        :: Day,
+    timeField1       :: TimeOfDay,
+    searchField1     :: Text,
+    urlField1        :: Text,
+    selectField1     :: Bool,
+    checkboxField1   :: Bool
+    }
+
 
 -- The datatype we wish to receive from the form
 data Person = Person
     { personName    :: Text
-    , personAge     :: Int
+    , personSurname :: Text
+    }
+    {--, personAge     :: Int
     , personColor   :: Color
     , personContent :: Textarea
-    }
+    --}
   deriving Show
 
+
 data Questions = Questions
-    { typ          :: Text
+    { typ           :: Text
     , name          :: Text
     , title         :: Text
     , colCount      :: Int
@@ -38,6 +63,48 @@ data Questions = Questions
 
 data Color = Red | Blue | Gray | Black
     deriving (Show, Eq, Enum, Bounded)
+
+
+hConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 2), submit = "Create user" }
+iConfig = BootstrapFormConfig { form = BootstrapInlineForm, submit = "Create user"}
+bConfig = BootstrapFormConfig { form = BootstrapBasicForm, submit = "Create user" }
+largeFormConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 2), submit = "Submit large data" }
+
+bootstrapFieldHelper config label placeholder = bootstrapFieldSettings config label Nothing placeholder Nothing Nothing
+
+personHForm :: Html -> MForm Handler (FormResult Person, Widget)
+personHForm = renderBootstrap hConfig $ Person
+    <$> areq textField (bootstrapFieldHelper hConfig "Name" (Just "Person name")) Nothing
+    <*> areq textField (bootstrapFieldHelper hConfig "Surname" (Just "Person surname")) Nothing
+
+personIForm :: Html -> MForm Handler (FormResult Person, Widget)
+personIForm = renderBootstrap iConfig $ Person
+    <$> areq textField (bootstrapFieldHelper iConfig "Name" (Just "Person name")) Nothing
+    <*> areq textField (bootstrapFieldHelper iConfig "Surname" (Just "Person surname")) Nothing
+
+personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm = renderBootstrap bConfig $ Person
+    <$> areq textField (bootstrapFieldHelper bConfig "Name" (Just "Person name")) Nothing
+    <*> areq textField (bootstrapFieldHelper bConfig "Surname" (Just "Person surname")) Nothing
+
+largeDataForm :: Html -> MForm Handler (FormResult LargeData, Widget)
+largeDataForm = renderBootstrap largeFormConfig $ LargeData
+    <$> areq textField (bootstrapFieldHelper hConfig "Text" (Just "Some text content")) Nothing
+    <*> areq intField (bootstrapFieldHelper hConfig "Int" (Just "Some integer value")) Nothing
+    <*> areq doubleField (bootstrapFieldHelper hConfig "Double" (Just "Some double value")) Nothing
+    <*> areq textareaField (bootstrapFieldHelper hConfig "Area" (Just "Some text area content")) Nothing
+    <*> areq hiddenField (bootstrapFieldHelper hConfig "Hidden" (Just "Hidden field")) Nothing
+    <*> areq passwordField (bootstrapFieldHelper hConfig "Password" (Just "Password field")) Nothing
+    <*> areq emailField (bootstrapFieldHelper hConfig "Email" (Just "Email field")) Nothing
+    <*> areq htmlField (bootstrapFieldHelper hConfig "Html" (Just "Some HTML")) Nothing
+    <*> areq dayField (bootstrapFieldHelper hConfig "Day" (Just "Some day")) Nothing
+    <*> areq timeField (bootstrapFieldHelper hConfig "Time" (Just "Some time")) Nothing
+    <*> areq (searchField False) (bootstrapFieldHelper hConfig "Search" (Just "Some search")) Nothing
+    <*> areq urlField (bootstrapFieldHelper hConfig "URL" (Just "Some URL")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Bool" (Just "Some bool")) Nothing
+    <*> areq checkBoxField (bootstrapFieldHelper hConfig "Checkbox" (Just "Some checkbox")) Nothing
+
+
 
 
 -- Declare the form. The type signature is a bit intimidating, but here's the
@@ -61,45 +128,40 @@ data Color = Red | Blue | Gray | Black
 -- > personForm :: Form Person
 --
 -- For our purposes, it's good to see the long version.
-personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderDivs $ Person
+{-- personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm = renderDivs $ Person 
     <$> areq textField "What's your name?" Nothing
     <*> areq intField "How old are you?" Nothing
     <*> areq (radioField optionsEnum) "Color" Nothing
     <*> areq textareaField "What is your current living situation?" Nothing
+--}
 
 -- The GET handler displays the form
 getQuizR :: Handler Html
 getQuizR = do
+    (basicWidget, enctype) <- generateFormPost personForm
+    (inlineWidget, enctype) <- generateFormPost personIForm
+    (horizontalWidget, enctype) <- generateFormPost personHForm
+    (largeWidget, enctype) <- generateFormPost largeDataForm
+    defaultLayout $ do
+        addStylesheetRemote "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
+        $(widgetFileReload def "Quiz")
+
     -- Generate the form to be displayed
-    (widget, enctype) <- generateFormPost personForm
+    {--(widget, enctype) <- generateFormPost personForm 
     defaultLayout
         [whamlet|
             <p>
                 The widget generated contains only the contents
                 of the form, not the form tag itself. So...
             <form method=post action=@{PersonR} enctype=#{enctype}>
-                ^{widget}
+                ^{widget}  
+            <form method=post action=@{PersonR} enctype=#{enctype}>
+                ^{widget}   
                 <p>It also doesn't include the submit button.
                 <button>Submit
-        |]
-
-question1R :: Handler Html 
-question1R = do 
-    -- Generate the form for the other question 
-        (widget, enctype) <- generateFormPost personForm 
-        defaultLayout
-            [whamlet|
-                <p>
-                    The widget generated contains only the contents
-                    of the form, not the form tag itself. So...
-                <form method=post action=@{PersonR} enctype=#{enctype}>
-                    ^{widget}
-                    <p>It also doesn't include the submit button.
-                    <button>Submit
-        |]
-
-
+        |] 
+--}
 -- The POST handler processes the form. If it is successful, it displays the
 -- parsed person. Otherwise, it displays the form again with error messages.
 
@@ -116,16 +178,3 @@ postPersonR = do
                     <button>Submit
             |]
 
-
-postQuestionR :: Handler Html
-postQuestionR = do 
-    ((result, widget), enctype) <- runFormPost personForm
-    case result of
-        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
-        _ -> defaultLayout
-            [whamlet|
-                <p>Invalid input, let's try again.
-                <form method=post action=@{PersonR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Submit
-            |]
