388eed712f425201d28e32f77884b6e33ea7e05b
kondylidou <kondylidou@cip.ifi.lmu.de>
2020-09-03 16:26:23 +0200

    quiz questions

--- src/Handler/Quiz.hs
+++ src/Handler/Quiz.hs
@@ -20,7 +20,7 @@ import Yesod.Form.Jquery
 import Form.Bootstrap3
 
 data LargeData = LargeData {
-    textField1       :: Text,
+    name             :: Text,
     age              :: Int,
     living           :: Bool,
     care             :: Bool,
@@ -30,15 +30,14 @@ data LargeData = LargeData {
     conf             :: Bool,
     fever            :: Bool,
     fever4           :: Bool,
-    checkboxField1   :: Bool,
-    color            :: [Text]
+    symptoms         :: Symptoms
     }
 
 
 -- The datatype we wish to receive from the form
 data Person = Person
-    { personName    :: Text
-    , personSurname :: Text
+    { personName    :: Bool
+    , personSurname :: Bool
     }
     {--, personAge     :: Int
     , personColor   :: Color
@@ -47,12 +46,16 @@ data Person = Person
   deriving Show
 
 
-data Questions = Questions
-    { typ           :: Text
-    , name          :: Text
-    , title         :: Text
-    , colCount      :: Int
-    , isRequired    :: Bool
+data Symptoms = Symptoms
+    { chills        :: Maybe Bool
+    , aches         :: Maybe Bool
+    , loss          :: Maybe Bool
+    , tired         :: Maybe Bool
+    , cough         :: Maybe Bool
+    , nose          :: Maybe Bool
+    , diarrhea      :: Maybe Bool
+    , throat        :: Maybe Bool
+    , headache      :: Maybe Bool
     }
     deriving Show
 
@@ -63,44 +66,50 @@ bConfig = BootstrapFormConfig { form = BootstrapBasicForm, submit = "Create user
 largeFormConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 4), submit = "Submit large data" }
 
 bootstrapFieldHelper config label placeholder = bootstrapFieldSettings config label Nothing placeholder Nothing Nothing
-
+{--
 personHForm :: Html -> MForm Handler (FormResult Person, Widget)
 personHForm = renderBootstrap hConfig $ Person
-    <$> areq textField (bootstrapFieldHelper hConfig "Name" (Just "Person name")) Nothing
-    <*> areq textField (bootstrapFieldHelper hConfig "Surname" (Just "Person surname")) Nothing
+    <$> areq checkBoxField (bootstrapFieldHelper hConfig "Name" (Just "Person name")) Nothing
+    <*> areq checkBoxField (bootstrapFieldHelper hConfig "Surname" (Just "Person surname")) Nothing
 
 
 
 personIForm :: Html -> MForm Handler (FormResult Person, Widget)
 personIForm = renderBootstrap iConfig $ Person
-    <$> areq textField (bootstrapFieldHelper iConfig "Name" (Just "Person name")) Nothing
-    <*> areq textField (bootstrapFieldHelper iConfig "Surname" (Just "Person surname")) Nothing
+    <$> areq checkBoxField (bootstrapFieldHelper iConfig "Name" (Just "Person name")) Nothing
+    <*> areq checkBoxField (bootstrapFieldHelper iConfig "Surname" (Just "Person surname")) Nothing
 
 
 personForm :: Html -> MForm Handler (FormResult Person, Widget)
 personForm = renderBootstrap bConfig $ Person
-    <$> areq textField (bootstrapFieldHelper bConfig "Name" (Just "Person name")) Nothing
-    <*> areq textField (bootstrapFieldHelper bConfig "Surname" (Just "Person surname")) Nothing
-
+    <$> areq checkBoxField (bootstrapFieldHelper bConfig "Name" (Just "Person name")) Nothing
+    <*> areq checkBoxField (bootstrapFieldHelper bConfig "Surname" (Just "Person surname")) Nothing
+--}
 
 largeDataForm :: Html -> MForm Handler (FormResult LargeData, Widget)
 largeDataForm = renderBootstrap largeFormConfig $ LargeData
-    <$> areq textField (bootstrapFieldHelper hConfig "Text" (Just "Some text content")) Nothing
+    <$> areq textField (bootstrapFieldHelper hConfig "What is your name?" (Just "Some name")) Nothing
     <*> areq intField (bootstrapFieldHelper hConfig "How old are you?" (Just "0")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Are you living alone?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "At least once a week, do you privately care for people with age-related conditions, chronic illnesses, or frailty?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Do you work in the medical field or in a community facility?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Do you smoke?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Is there a pregnancy possibility?" (Just "Some bool")) Nothing
+    <*> areq boolField (bootstrapFieldHelper hConfig "Are you pregnant?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Have you had close contact with a confirmed case?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "In the past 4 days, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
-    <*> areq checkBoxField (bootstrapFieldHelper hConfig "Checkbox" (Just "Some checkbox")) Nothing
-    <*> areq (multiSelectFieldList $ colors) "Color" Nothing
+    <*> symptoms
     where
-        colors :: [Text]
-        colors = ["Red", "Black"]
-
+        symptoms = Symptoms
+            <$> aopt checkBoxField (bootstrapFieldHelper hConfig "Chills" (Just "Person chills")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Body aches" (Just "Person aches")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Loss of taste or smell" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Feeling tired or weak" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Persistent cough" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Runny nose" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Diarrhea" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Sore throat" (Just "Person loss")) Nothing
+            <*> aopt checkBoxField (bootstrapFieldHelper hConfig "Headache" (Just "Person loss")) Nothing
 -- Declare the form. The type signature is a bit intimidating, but here's the
 -- overview:
 --
@@ -133,9 +142,9 @@ personForm = renderDivs $ Person
 -- The GET handler displays the form
 getQuizR :: Handler Html
 getQuizR = do
-    (basicWidget, enctype) <- generateFormPost personForm
-    (inlineWidget, enctype) <- generateFormPost personIForm
-    (horizontalWidget, enctype) <- generateFormPost personHForm
+    --(basicWidget, enctype) <- generateFormPost personForm
+    --(inlineWidget, enctype) <- generateFormPost personIForm
+    --(horizontalWidget, enctype) <- generateFormPost personHForm
     (largeWidget, enctype) <- generateFormPost largeDataForm
     defaultLayout $ do
         addStylesheetRemote "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
@@ -158,17 +167,4 @@ getQuizR = do
 --}
 -- The POST handler processes the form. If it is successful, it displays the
 -- parsed person. Otherwise, it displays the form again with error messages.
-
-postPersonR :: Handler Html
-postPersonR = do
-    ((result, widget), enctype) <- runFormPost personForm
-    case result of
-        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
-        _ -> defaultLayout
-            [whamlet|
-                <p>Invalid input, let's try again.
-                <form method=post action=@{PersonR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Submit
-            |]
 
