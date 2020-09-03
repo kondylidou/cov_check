6bcd5f52a6873e86b47799fbbd04b42ca8cc6564
Kevin Nsieyanji Tchokodeu <nsieyanjitchokodeu@cip.ifi.lmu.de>
2020-09-03 10:59:28 +0200

    add section title for each question

--- src/Handler/Quiz.hs
+++ src/Handler/Quiz.hs
@@ -30,8 +30,8 @@ data LargeData = LargeData {
     conf             :: Bool,
     fever            :: Bool,
     fever4           :: Bool,
-    checkboxField1   :: Bool,
-    color            :: [Text]
+    checkboxField1   :: Bool
+ --   colors            :: (Text,Text)
     }
 
 
@@ -39,11 +39,9 @@ data LargeData = LargeData {
 data Person = Person
     { personName    :: Text
     , personSurname :: Text
+    , personAge     :: Int
+    , personEmail   :: Text
     }
-    {--, personAge     :: Int
-    , personColor   :: Color
-    , personContent :: Textarea
-    --}
   deriving Show
 
 
@@ -64,23 +62,12 @@ largeFormConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2)
 
 bootstrapFieldHelper config label placeholder = bootstrapFieldSettings config label Nothing placeholder Nothing Nothing
 
-personHForm :: Html -> MForm Handler (FormResult Person, Widget)
-personHForm = renderBootstrap hConfig $ Person
+personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm = renderBootstrap hConfig $ Person
     <$> areq textField (bootstrapFieldHelper hConfig "Name" (Just "Person name")) Nothing
     <*> areq textField (bootstrapFieldHelper hConfig "Surname" (Just "Person surname")) Nothing
-
-
-
-personIForm :: Html -> MForm Handler (FormResult Person, Widget)
-personIForm = renderBootstrap iConfig $ Person
-    <$> areq textField (bootstrapFieldHelper iConfig "Name" (Just "Person name")) Nothing
-    <*> areq textField (bootstrapFieldHelper iConfig "Surname" (Just "Person surname")) Nothing
-
-
-personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderBootstrap bConfig $ Person
-    <$> areq textField (bootstrapFieldHelper bConfig "Name" (Just "Person name")) Nothing
-    <*> areq textField (bootstrapFieldHelper bConfig "Surname" (Just "Person surname")) Nothing
+    <*> areq intField (bootstrapFieldHelper hConfig "Age" (Just "0")) Nothing
+    <*> areq textField (bootstrapFieldHelper hConfig "Email" (Just "Person email")) Nothing
 
 
 largeDataForm :: Html -> MForm Handler (FormResult LargeData, Widget)
@@ -96,46 +83,12 @@ largeDataForm = renderBootstrap largeFormConfig $ LargeData
     <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "In the past 4 days, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
     <*> areq checkBoxField (bootstrapFieldHelper hConfig "Checkbox" (Just "Some checkbox")) Nothing
-    <*> areq (multiSelectFieldList $ colors) "Color" Nothing
-    where
-        colors :: [Text]
-        colors = ["Red", "Black"]
-
--- Declare the form. The type signature is a bit intimidating, but here's the
--- overview:
---
--- * The Html parameter is used for encoding some extra information. See the
--- discussion regarding runFormGet and runFormPost below for further
--- explanation.
---
--- * We have our Handler as the inner monad, which indicates which site this is
--- running in.
---
--- * FormResult can be in three states: FormMissing (no data available),
--- FormFailure (invalid data) and FormSuccess
---
--- * The Widget is the viewable form to place into the web page.
---
--- Note that the scaffolded site provides a convenient Form type synonym,
--- so that our signature could be written as:
---
--- > personForm :: Form Person
---
--- For our purposes, it's good to see the long version.
-{-- personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderDivs $ Person 
-    <$> areq textField "What's your name?" Nothing
-    <*> areq intField "How old are you?" Nothing
-    <*> areq (radioField optionsEnum) "Color" Nothing
-    <*> areq textareaField "What is your current living situation?" Nothing
---}
+ 
 
 -- The GET handler displays the form
 getQuizR :: Handler Html
 getQuizR = do
     (basicWidget, enctype) <- generateFormPost personForm
-    (inlineWidget, enctype) <- generateFormPost personIForm
-    (horizontalWidget, enctype) <- generateFormPost personHForm
     (largeWidget, enctype) <- generateFormPost largeDataForm
     defaultLayout $ do
         addStylesheetRemote "//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
