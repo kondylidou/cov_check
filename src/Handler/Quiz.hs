88a6158101d11b106363c2d41fe30d0b06132296
kondylidou <kondylidou@cip.ifi.lmu.de>
2020-09-11 14:38:37 +0200

    programm logic

--- src/Handler/Quiz.hs
+++ src/Handler/Quiz.hs
@@ -21,42 +21,51 @@ import qualified Data.Text as T
 import Form.Bootstrap3
 
 -- Define our data that will be used for creating the form
-data QuizData = QuizData {
-    name                :: Text, 
-    surname             :: Text, 
-    email               :: Text,
-    age                 :: Int, 
-    living              :: Bool,
-    care                :: Bool,
-    work                :: Bool,
-    smoke               :: Bool,
-    pregnancy           :: Bool,
-    conf                :: Bool,
-    fever               :: Bool,
-    fever4              :: Bool,
-    chills              :: Bool,
-    aches               :: Bool,
-    loss                :: Bool,
-    tired               :: Bool,
-    cough               :: Bool,
-    nose                :: Bool,
-    diarrhea            :: Bool,
-    throat              :: Bool,
-    headache            :: Bool,
-    breath              :: Bool,
-    --day                 :: Maybe Day,
-    lung                :: Bool,
-    diabetes            :: Bool,
-    heart               :: Bool,
-    obesity             :: Bool,
-    steroids            :: Bool,
-    immunosuppressants  :: Bool,
-    flu                 :: Bool
+data QuizData = QuizData 
+    { name                :: Text
+    , surname             :: Text
+    , email               :: Text
+    , age                 :: Int
+    , living              :: Bool
+    , care                :: Bool
+    , work                :: Bool
+    , smoke               :: Bool
+    , pregnancy           :: Bool
+    , conf                :: Bool
+    , symptoms            :: Symptoms
+    , record              :: Record
     }
    deriving (Show,Generic)
 
 
-instance FromJSON QuizData
+data Symptoms = Symptoms
+    { fever         :: Bool
+    , fever4        :: Bool
+    , chills        :: Bool
+    , aches         :: Bool
+    , loss          :: Bool
+    , tired         :: Bool
+    , cough         :: Bool
+    , nose          :: Bool
+    , diarrhea      :: Bool
+    , throat        :: Bool
+    , headache      :: Bool
+    , breath        :: Bool
+    }
+    deriving Show
+
+
+data Record = Record
+    { lung                :: Bool
+    , diabetes            :: Bool
+    , heart               :: Bool
+    , obesity             :: Bool
+    , steroids            :: Bool
+    , immunosuppressants  :: Bool
+    , flu                 :: Bool
+    }
+    deriving Show
+
 
 hConfig = BootstrapFormConfig { form = BootstrapHorizontalForm (ColXs 2) (ColXs 4) (ColXs 2), submit = "Create user" }
 iConfig = BootstrapFormConfig { form = BootstrapInlineForm, submit = "Create user"}
@@ -78,29 +87,31 @@ quizDataForm = renderBootstrap largeFormConfig $ QuizData
     <*> areq boolField (bootstrapFieldHelper hConfig "Do you smoke?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Are you pregnant?" (Just "Some bool")) Nothing
     <*> areq boolField (bootstrapFieldHelper hConfig "Have you had close contact with a confirmed case?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "In the past 4 days, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Chills" (Just "Person chills")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Body aches" (Just "Person aches")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Loss of taste or smell" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Feeling tired or weak" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Persistent cough" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Runny nose" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Diarrhea" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Sore throat" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Headache" (Just "Person loss")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, did you feel that you were more quickly out of breath than usual?" (Just "Some bool")) Nothing
-    -- <*> aopt (jqueryDayField def
-    --    { jdsChangeYear = True -- give a year dropdown
-    --    , jdsYearRange = "1900:" -- 1900 till 0 years ago
-    --    }) "With regard to all questions about symptoms: since when have you had the symptoms you specified?" Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with chronic lung disease by a doctor?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with diabetes by a doctor?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with heart disease by a doctor?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with obesity by a doctor?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Are you currently taking steroids?" (Just "Some bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Are you currently taking immunosuppressants?" (Just "bool")) Nothing
-    <*> areq boolField (bootstrapFieldHelper hConfig "Have you been vaccinated against flu between October 2019 and today?" (Just "Some bool")) Nothing
+    <*> symptoms
+    <*> record
+    where
+        symptoms = Symptoms
+            <$> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "In the past 4 days, have you had a fever (over 38°C)?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Chills" (Just "Person chills")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Body aches" (Just "Person aches")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Loss of taste or smell" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Feeling tired or weak" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Persistent cough" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Runny nose" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Diarrhea" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Sore throat" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Headache" (Just "Person loss")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "In the past 24 hours, did you feel that you were more quickly out of breath than usual?" (Just "Some bool")) Nothing
+        record = Record
+            <$> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with chronic lung disease by a doctor?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with diabetes by a doctor?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with heart disease by a doctor?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Have you been diagnosed with obesity by a doctor?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Are you currently taking steroids?" (Just "Some bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Are you currently taking immunosuppressants?" (Just "bool")) Nothing
+            <*> areq boolField (bootstrapFieldHelper hConfig "Have you been vaccinated against flu between October 2019 and today?" (Just "Some bool")) Nothing
+
 
 -- The GET handler displays the form
 getQuizR :: Handler Html
@@ -119,19 +130,44 @@ postQuizR = do
     ((result, quizWidget), enctype) <- runFormPost quizDataForm
     let handlerName = "postQuizR" :: Text
         submission = case result of
-            FormSuccess res ->  case processData res of 
+            FormSuccess res -> case (conf res) of 
                 True -> Just res
-                _ -> Nothing
+                _ -> case processSymptoms (symptoms res) of
+                    True -> Just res
+                    _ -> case processRecord (record res) of
+                        True -> Just res
+                        _ ->  Nothing
             _ -> Nothing
 
     defaultLayout $ do
         setTitle "Quiz Result"
         $(widgetFile "quiz")
 
-processData :: QuizData -> Bool
-processData quizdata 
-    | fever quizdata == True = True
-    | fever4 quizdata == True = True
+processSymptoms :: Symptoms -> Bool
+processSymptoms symptoms 
+    | fever symptoms == True = True
+    | fever4 symptoms == True = True
+    | cough symptoms && tired symptoms == True = True
+    | cough symptoms && diarrhea symptoms == True = True
+    | cough symptoms && headache symptoms == True = True
+    | cough symptoms && loss symptoms == True = True
+    | tired symptoms && diarrhea symptoms == True = True
+    | tired symptoms && headache symptoms == True = True
+    | tired symptoms && loss symptoms == True = True
+    | diarrhea symptoms && loss symptoms == True = True
+    | diarrhea symptoms && headache symptoms == True = True
+    | headache symptoms && loss symptoms == True = True
+    | otherwise = False
+
+processRecord :: Record -> Bool
+processRecord record 
+    | lung record == True = True
+    | diabetes record == True = True
+    | heart record == True = True
+    | obesity record == True = True
+    | steroids record == True = True
+    | immunosuppressants record == True = True
+    | flu record == True = True
     | otherwise = False
 
 
